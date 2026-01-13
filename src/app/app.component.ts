3688316fcd17aa645953e693609b9afdbad18528
Mohamad Chukr <W0869957@myscc.ca>
2026-01-13 14:15:06 -0800

    Created starter Angular file and completed Assignment 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,11 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
 export class App {
   protected readonly title = signal('Mohamad_Chukr_Learning_Angular');
+  firstName: string = 'Mohamad';
+  age: number = 19;
 }
